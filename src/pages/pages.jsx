4171cfb3c83e75214713aca3f00cae88f1eca237
shinjihye <twg0226@gmail.com>
2023-11-27 23:37:11 +0900

    경로, 컴포넌트 모듈 작업

---
+++ src/pages/pages.jsx
@@ -0,0 +1,5 @@
+export { default as Diary } from './diary';
+export { default as Community } from './community';
+export { default as HealthInfo } from './health_info';
+export { default as Medical_sponsorship } from './medical_sponsorship';
+export { default as Mypage } from './mypage';
---
+++ src/pages/pages_paths.jsx
@@ -0,0 +1,7 @@
+export const BottomNavPaths = {
+    DIARY: "pages/diary",
+    COMMUNITY: "pages/commynity",
+    MEDICAL_SPONSORSHIP: "pages/modeical_sposorship",
+    HEALTH_INFO: "pages/health_info",
+    MYPAGE: "pages/mypage",
+};

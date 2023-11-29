77e45e341d9c9dcfe392cdf3b17ad900a9595842
shinjihye <twg0226@gmail.com>
2023-11-30 00:33:53 +0900

    메인 페이지들 paths 작업

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -1,5 +1,5 @@
 export { default as Diary } from './diary';
 export { default as Community } from './community';
 export { default as HealthInfo } from './health_info';
-export { default as Medical_sponsorship } from './medical_sponsorship';
+export { default as MedicalSponsorship } from './medical_sponsorship';
 export { default as Mypage } from './mypage';

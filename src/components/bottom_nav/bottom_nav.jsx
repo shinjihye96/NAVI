aef1765976a99baad617acff3fe46eee69ccfaaa
shinjihye <twg0226@gmail.com>
2023-12-04 22:07:08 +0900

    바텀 탭 ui 작업

--- src/components/bottom_nav/bottom_nav.jsx
+++ src/components/bottom_nav/bottom_nav.jsx
@@ -8,15 +8,28 @@ export default function BottomNav() {
     const bottomNavLength = Object.values(BottomNavPaths);
     const [currentTab, setCurrentTab] = useState(0);
 
+    const navIcons = [Icon.Write, Icon.Chat, Icon.Book, Icon.Hospital, Icon.Person];
+    const navActiveIcons = [Icon.WriteFill, Icon.ChatFill, Icon.BookFill, Icon.HospitalFill, Icon.PersonFill];
+    const navName = ['하루공유', '커뮤니티', '건강정보', '의료∙후원', '내정보'];
+
+    const changeNav = (index) => {
+        setCurrentTab(index);
+    }
+
     return (
         <>
             <nav className="nav_wrap">
                 <ul className="nav_tab">
                     {bottomNavLength.map((path, index) => (
                         <li key={index} className='nav_tab_con'>
-                            <NavLink to={path} className='con'>
-                                <Icon.Bell size={24} className='nav_icon'/>
-                                <p className='nav_name'>{index}</p>
+                            <NavLink
+                                to={path}
+                                className={`con ${index === currentTab ? 'active' : ''}`} onClick={() => changeNav(index)}
+                            >
+                                {React.createElement(
+                                    index === currentTab ? navActiveIcons[index] : navIcons[index], { size: 24, className: 'nav_icon' })
+                                }
+                                <p className='nav_name'>{navName[index]}</p>
                             </NavLink>
                         </li>
                     ))}

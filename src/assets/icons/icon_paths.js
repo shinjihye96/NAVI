bdb56e787adb68bd0ed26be3a2f970db4d350864
shinjihye <twg0226@gmail.com>
2023-12-04 21:48:19 +0900

    아이콘명 수정

--- src/assets/icons/icon_paths.js
+++ src/assets/icons/icon_paths.js
@@ -1,36 +1,42 @@
-export { default as Arrowleft } from './arrow_left';
+export { default as ArrowLeft } from './arrow_left';
 export { default as Bell } from './bell';
 export { default as BookFill } from './book_fill';
 export { default as Book } from './book';
 export { default as Camera } from './camera';
-export { default as Chatfill } from './chat_fill';
+export { default as ChatFill } from './chat_fill';
 export { default as Chat } from './chat';
-export { default as Circledown } from './chevron_down';
-export { default as Chevronup } from './chevron_up';
-export { default as Chevronleft } from './chevron_left';
-export { default as Chevronright } from './chevron_right';
+export { default as CircleDown } from './chevron_down';
+export { default as ChevronUp } from './chevron_up';
+export { default as ChevronLeft } from './chevron_left';
+export { default as ChevronRight } from './chevron_right';
 export { default as Clock } from './clock';
-export { default as Closecirclefill } from './close_circle_fill';
-export { default as Closecircle } from './close_circle';
+export { default as CloseCircleFill } from './close_circle_fill';
+export { default as CloseCircle } from './close_circle';
 export { default as Close } from './close';
 export { default as Edit } from './edit';
 export { default as Emotionminus } from './emotion_minus';
 export { default as Emotionplus } from './emotion_plus';
-export { default as Eyeclose } from './eye_close';
-export { default as Eyeopen } from './eye_open';
+export { default as EyeClose } from './eye_close';
+export { default as EyeOpen } from './eye_open';
 export { default as Follower } from './follower';
-export { default as Heartfill } from './heart_fill';
+export { default as HeartFill } from './heart_fill';
 export { default as Heart } from './heart';
-export { default as Hospitalfill } from './hospital_fill';
+export { default as HospitalFill } from './hospital_fill';
 export { default as Hospital } from './hospital';
 export { default as Image } from './image';
-export { default as Keyboarddown } from './keyboard_down';
-export { default as Mailfill } from './mail_fill';
+export { default as KeyboardDown } from './keyboard_down';
+export { default as MailFill } from './mail_fill';
 export { default as Mail } from './mail';
-export { default as Personfill } from './person_fill';
+export { default as PersonFill } from './person_fill';
 export { default as Person } from './person';
 export { default as Plus } from './plus';
 export { default as Search } from './search';
 export { default as Sort } from './sort';
-export { default as Writefill } from './write_fill';
+export { default as WriteFill } from './write_fill';
 export { default as Write } from './write';
+
+// example
+{/* <Icon.ArrowLeft size={24} clasName='icon_example' > */}
+
+// icon color example
+// .icon_example: fill: $Gray100;

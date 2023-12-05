4619f4f78c9c7c636e9495087bfcc1dcc3b59e3d
shinjihye <twg0226@gmail.com>
2023-12-05 23:45:02 +0900

    bottom nav 아이콘 수정

--- src/components/bottom_nav/bottom_nav.jsx
+++ src/components/bottom_nav/bottom_nav.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BottomNavPaths } from 'pages/pages_paths';
-import * as Icon from 'assets/icons/icon_paths';
+import Icon from 'styles/components/icon';
 import './bottom_nav.scss';
 
 export default function BottomNav() {
     const bottomNavLength = Object.values(BottomNavPaths);
     const [currentTab, setCurrentTab] = useState(0);
 
-    const navIcons = [Icon.Write, Icon.Chat, Icon.Book, Icon.Hospital, Icon.Person];
-    const navActiveIcons = [Icon.WriteFill, Icon.ChatFill, Icon.BookFill, Icon.HospitalFill, Icon.PersonFill];
+    const navIcons = ['Write', 'Chat', 'Book', 'Hospital', 'Person'];
+    const navActiveIcons = ['WriteFill', 'ChatFill', 'BookFill', 'HospitalFill', 'PersonFill'];
     const navName = ['하루공유', '커뮤니티', '건강정보', '의료∙후원', '내정보'];
 
     const changeNav = (index) => {
@@ -24,11 +24,9 @@ export default function BottomNav() {
                         <li key={index} className='nav_tab_con'>
                             <NavLink
                                 to={path}
-                                className={`con ${index === currentTab ? 'active' : ''}`} onClick={() => changeNav(index)}
+                                className={`con`} onClick={() => changeNav(index)}
                             >
-                                {React.createElement(
-                                    index === currentTab ? navActiveIcons[index] : navIcons[index], { size: 24, className: 'nav_icon' })
-                                }
+                                <Icon name={index === currentTab ? navActiveIcons[index] : navIcons[index]} size={24} className='nav_icon' />
                                 <p className='nav_name'>{navName[index]}</p>
                             </NavLink>
                         </li>

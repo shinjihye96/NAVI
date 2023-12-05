9cad765830d261e0e11ae118abf3d3a7d490622b
shinjihye <twg0226@gmail.com>
2023-12-06 00:03:25 +0900

    svg code fillRule clipRule 수정

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import { PrimButton, TextButton, IconButton } from 'styles/components/button'
+import React from 'react';
+import { PrimButton } from 'styles/components/button'
+import Icon from 'styles/components/icon';
+import 'styles/components/button.scss'
 
 export default function Diary() {
     return(
         <>
             <div className="">다이어리</div>
             <div className="">
-                <PrimButton title='ㅅㄷㄴㅅ' iconPosition='left' />
+                <Icon name='ArrowLeft' className='1'/>
+                <PrimButton title='test' iconPosition='left' icon='Plus' type='primary'/>
             </div>
         </>
     );

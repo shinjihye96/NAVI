b74deabce6474c6852057f3b70ebe963a1cd62bf
shinjihye <twg0226@gmail.com>
2023-12-11 20:33:49 +0900

    버튼 테스트

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { PrimButton } from 'styles/components/button'
-import Icon from 'styles/components/icon';
+import { PrimButton, TextButton, IconButton } from 'styles/components/button'
 import 'styles/components/button.scss'
 
 export default function Diary() {
@@ -8,8 +7,21 @@ export default function Diary() {
         <>
             <div className="">다이어리</div>
             <div className="">
-                <Icon name='ArrowLeft' className='1'/>
-                <PrimButton title='test' iconPosition='left' icon='Plus' type='primary'/>
+                <PrimButton title='test L' size='L' iconPosition='left' icon='Plus' type='primary' />
+                <PrimButton title='test M' size='M' iconPosition='right' icon='Follower' type='secondary' />
+                <PrimButton title='test S' size='S' iconPosition='left' icon='Plus' type='glass' />
+                <PrimButton href={`/`} size='M' title='test경로가있음 M' iconPosition='left' icon='Plus' type='primary' />
+                <p>ㅋㅋ</p>
+                <TextButton title='test L' size='L' iconPosition='left' icon='Plus' type='primary' />
+                <TextButton title='test M' size='M' iconPosition='right' icon='Follower' type='secondary' />
+                <TextButton title='test S' size='S' iconPosition='left' icon='Plus' type='glass' />
+                <TextButton title='test XS' size='XS' iconPosition='left' icon='Plus' type='glass' />
+                <TextButton href={`/`} title='test경로가있음' iconPosition='left' icon='Plus' type='primary' />
+                <p>zz</p>
+                {/* <IconButton iconName='Plus' type='primary' size='L' />
+                <IconButton iconName='Follower' type='secondary' size='M' />
+                <IconButton iconName='Plus' type='glass' size='S' />
+                <IconButton href={`/`} icon='Plus' type='primary' size='L'/> */}
             </div>
         </>
     );

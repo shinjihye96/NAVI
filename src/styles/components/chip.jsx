8cc9ba7c09e7d4eb4f40c098f461b23853088998
shinjihye <twg0226@gmail.com>
2024-01-02 20:23:19 +0900

    chip 최종수정

--- src/styles/components/chip.jsx
+++ src/styles/components/chip.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './chip.scss';
-export default function Chips({icon, name, className, onclick, ...props}) {
+export default function Chips({icon, text, className = '', onclick, ...props}) {
     const isOnclick = onclick;
 
     if(isOnclick) {
         return (
             <button className={`chip ${className}`} onClick={onclick} {...props}>
             <img src={`${icon}`} className='icon' alt='chip' />
-            {name && <p className='txt'>{name}</p>}
+            {text !=undefined && <p className='txt'>{text}</p>}
         </button>
         );
     }
     return(
         <div className={`chip ${className}`} {...props}>
             <img src={`${icon}`} className='icon' alt='chip_icon' />
-            {name && <p className='txt'>{name}</p>}
+            {text !=undefined && <p className='txt'>{text}</p>}
         </div>
     );
 }

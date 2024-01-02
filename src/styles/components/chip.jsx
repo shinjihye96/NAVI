df331924297b2cb76acf974914e43c704eb345a7
shinjihye <twg0226@gmail.com>
2024-01-02 19:51:32 +0900

    컴포넌트 수정

--- src/styles/components/chip.jsx
+++ src/styles/components/chip.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './chip.scss';
-export default function Chips({icon, name, onclick, ...props}) {
+export default function Chips({icon, name, className, onclick, ...props}) {
     const isOnclick = onclick;
 
     if(isOnclick) {
         return (
-            <button className="chip" onClick={onclick} {...props}>
+            <button className={`chip ${className}`} onClick={onclick} {...props}>
             <img src={`${icon}`} className='icon' alt='chip' />
             {name && <p className='txt'>{name}</p>}
         </button>
         );
     }
     return(
-        <div className="chip" {...props}>
+        <div className={`chip ${className}`} {...props}>
             <img src={`${icon}`} className='icon' alt='chip_icon' />
             {name && <p className='txt'>{name}</p>}
         </div>

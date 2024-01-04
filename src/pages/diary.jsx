93b9ad0f66b6f77cca4dc5daae92671e43cc9da1
shinjihye <twg0226@gmail.com>
2024-01-05 00:08:15 +0900

    임시 ui 적용

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -1,7 +1,37 @@
+import React from 'react';
+import dayjs from 'dayjs'
+import 'dayjs/locale/ko';
+import './diary.scss';
+import { IconButton } from 'styles/components/button';
+import MyMood from 'components/diary/mood'
+import DiaryList from 'components/diary/list'
+
+const today = dayjs().format('YYYY년 MM월 DD일');
 export default function Diary() {
+    const follower = () => {
+        console.log('Follower');
+    }
+
+    const notification = () => {
+        console.log('notification');
+    }
+
     return(
         <>
-            <div className="">다이어리</div>
+            <img src="./img/dayshare_top_bg.png" className='diary_bg' alt="diary_bg" />
+            <div className="diary_head">
+                <p className='today'>{today}</p>
+                <div className="btn_box">
+                    <IconButton iconName='Follower' type='quaternary' size='L' onClick={follower}/>
+                    <IconButton iconName='Bell' type='quaternary' size='L' onClick={notification}/>
+                </div>
+            </div>
+            <MyMood />
+            <DiaryList />
+            <DiaryList />
+            <DiaryList />
+            <DiaryList />
+            <DiaryList />
         </>
     );
 }

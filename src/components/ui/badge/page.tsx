69a748aca35ab63ca21184a8b42df11bead769df
shinjihye <twg0226@gmail.com>
2025-02-18 16:56:36 +0900

    badge ui 작업

---
+++ src/components/ui/badge/page.tsx
@@ -0,0 +1,18 @@
+interface BadgeProps{
+    className?: string;
+    txt: string;
+    color: boolean;
+}
+export default function Badge({
+    className = '',
+    txt = '',
+    color = false,
+}: BadgeProps){
+    return(
+        <>
+            <div className={`${color ? 'bg-semantic-r300' : 'bg-gray-200'} rounded-[99rem] px-[3rem] ${className}`}>
+                <span className={`font-regular text-[10rem] leading-[14rem] ${color ? 'text-gray-700' : 'text-base-wtf'}`}>{txt}</span>
+            </div>
+        </>
+    );
+}

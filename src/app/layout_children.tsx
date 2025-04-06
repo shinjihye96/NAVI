c1cf79b1f14b22ab5ac2eab18d591543668936ee
shinjihye <twg0226@gmail.com>
2025-04-06 23:21:31 +0900

    클래스 정리리

--- src/app/layout_children.tsx
+++ src/app/layout_children.tsx
@@ -11,7 +11,7 @@ const LayoutChildren: React.FC<LayoutContainerProps> =({children}) => {
 
     return(
         <main className='w-[360rem] m-auto'>
-            <section className='bg-tp-bk8'>
+            <section className=''>
                 {children}
             </section>
             <BottomNav />

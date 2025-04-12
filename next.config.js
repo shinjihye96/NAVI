9fa1b268daad6009fc8520b4ed269051df90c13f
shinjihye <twg0226@gmail.com>
2025-04-13 03:02:06 +0900

    api 주소 수정정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/daily-share',
+            destination: 'http://localhost:3000/daily-share',
+          },
+        ];
+    },
+}
 
 module.exports = nextConfig

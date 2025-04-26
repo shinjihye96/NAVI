73b36d71353c820b9903b2cff855475bd982c432
shinjihye <twg0226@gmail.com>
2025-04-26 15:37:42 +0900

    스타일 수정

--- src/components/appBar/page.tsx
+++ src/components/appBar/page.tsx
@@ -7,6 +7,7 @@ interface AppBarProps {
   left?: React.ReactNode;
   right?: React.ReactNode;
   center?: React.ReactNode;
+  sticky?: boolean;
 }
 
 export default function AppBar({
@@ -14,9 +15,10 @@ export default function AppBar({
   left,
   right,
   center,
+  sticky = true,
 }: AppBarProps) {
   return (
-    <header className="h-[56px] px-4 flex items-center border-b sticky top-0 z-10 bg-base-wf">
+    <header className={`h-[56px] px-4 flex items-center border-b bg-base-wf ${sticky && 'sticky top-0 z-10'}`}>
       <div className="grid items-center grid-cols-[1fr_2fr_1fr] w-full">
         <div className="flex justify-start">{left}</div>
         <div className="flex justify-center">

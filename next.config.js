c920b1fa4932f796312bf6dfa3d31b94218fec5e
shinjihye <twg0226@gmail.com>
2025-04-26 15:33:01 +0900

    API url 초기세팅팅

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 const nextConfig = {
     async rewrites() {
         return [
+          // {
+          //   source: '/api/daily-share',
+          //   destination: 'http://localhost:3000/daily-share',
+          // },
           {
-            source: '/api/daily-share',
-            destination: 'http://localhost:3000/daily-share',
+            source: '/api/:path*',
+            destination: 'http://localhost:3000/:path*',
           },
         ];
     },

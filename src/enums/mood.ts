49573f8470f77f1c5abc839f7a6c052717b45350
shinjihye <twg0226@gmail.com>
2025-04-27 22:42:54 +0900

    코드정리리

--- src/enums/mood.ts
+++ src/enums/mood.ts
@@ -1,9 +1,9 @@
 export enum MoodType {
-    NONE          = 'NONE',
-    SUN           = 'SUN',
+    NONE = 'NONE',
+    SUN = 'SUN',
     SUN_AND_CLOUD = 'SUN_AND_CLOUD',
-    CLOUD         = 'CLOUD',
-    RAIN          = 'RAIN',
-    LIGHTNING     = 'LIGHTNING',
+    CLOUD = 'CLOUD',
+    RAIN = 'RAIN',
+    LIGHTNING = 'LIGHTNING',
 }
   

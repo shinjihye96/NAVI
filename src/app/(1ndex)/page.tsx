d9e0126f6d3054a0a10087cf5b510ee8e911001e
shinjihye <twg0226@gmail.com>
2025-05-28 22:34:26 +0900

    메인페이지 작업업

--- src/app/(1ndex)/page.tsx
+++ src/app/(1ndex)/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import DailyShare from "@/page/(daily)/daily_share/page";
 import { Button, IconButton, TextButton } from "components/ui/button/page";
 import Chips from "components/ui/chip/page";
 import Input from "components/ui/input/page";
@@ -10,31 +11,7 @@ export default function Home() {
   const [value, setValue] = useState('')
   return (
     <main>
-      <Search
-        value={value}
-        name="search"
-        onChange={() => {}}
-      />
-      <Input
-        label="label"
-        name="vlue"
-        value="dmdpdpd"
-        onChange={() => {}}      
-      />
-      <Input
-        label="label"
-        name="vlue"
-        value="dmdpdpd"
-        type="line"
-        onChange={() => {}}      
-      />
-      <Input
-        label="label"
-        name="vlue"
-        value="dmdpdpd"
-        type="noLine"
-        onChange={() => {}}      
-      />
+      <DailyShare />
     </main>
   )
 }

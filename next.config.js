f6a11491a0f0e140c0a6650e723b6a113d181463
shinjihye96 <twg0226@gmail.com>
2026-01-15 15:23:15 +0900

    fix: correct API destination URL in rewrites to include '/api' prefix

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
           // },
           {
             source: '/api/:path*',
-            destination: 'http://localhost:3003/:path*',
+            destination: 'http://localhost:3003/api/:path*',
           },
         ];
     },
